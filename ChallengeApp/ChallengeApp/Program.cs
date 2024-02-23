8aec0268fb9af63cb5b73c20cc63cdfa05002314
Domix1997 <domix1221@gmail.com>
2024-02-23 19:57:02 +0100

    Zadanie dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Dominik";
+var sex = "Mężczyzna";
+var age = 16;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mężczyzna");
+}
